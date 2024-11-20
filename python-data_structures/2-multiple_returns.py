e92568db64b78723498ea1f4770694f450365cf9
Muradd0507 <muradqulizade75@mail.com>
2024-11-20 16:05:58 -0500

    project7_2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup1 = (len(sentence),)
-    tup2 = (sentence[0],)
+    if sentence != "":
+        tup1 = (len(sentence),)
+        tup2 = (sentence[0],)
+    else:
+        tup1 = (len(sentence),)
+        tup2 = None
     tup = tup1 + tup2
     return tup
