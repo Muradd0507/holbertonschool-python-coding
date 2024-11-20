0d1604e4ddd2fb372c6ea3737a674c73d1ad5c12
Muradd0507 <muradqulizade75@mail.com>
2024-11-20 16:10:24 -0500

    project7_2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -3,6 +3,7 @@ def multiple_returns(sentence):
     if sentence != "":
         tup1 = (len(sentence),)
         tup2 = (sentence[0],)
+        tup = tup1 + tup2
         return tup
     else:
         print("Length: {} -First character: {}".format(len(string), None))
