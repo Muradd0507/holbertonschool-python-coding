90cceb29fa1304d1a893c53209f81ba8eeb4faf4
Muradd0507 <muradqulizade75@mail.com>
2024-11-20 15:34:35 -0500

    project7_1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     k = 1
+    m = 1
     for i in matrix:
         for j in i:
-            if k % 3 != 0:
-                print("{:d}".format(j), end=" ")
-            else:
+            if k != m:
+                m = k
                 print("{:d}".format(j), end="\n")
+            else:
+                print("{:d}".format(j), end=" ")
             k += 1
