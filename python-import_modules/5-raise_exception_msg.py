f7f368bb4aafa4990783e37d84c578e7037f968d
Muradd0507 <muradqulizade75@mail.com>
2024-11-20 13:59:35 -0500

    project6_5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
-    if message=="C is fun":
+def raise_exception_msg(message = ""):
+    if message == "C is fun":
         raise NameError("C is fun")
     else:
         print(message)
+    return 0
