db6a1690146c0b67fdd44ea5cac015d884792a7e
Muradd0507 <muradqulizade75@mail.com>
2024-11-20 10:44:02 -0500

    project6_3

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        print(a/b)
+    except ZeroDivisionError:
+        None
+    finally:
+        print("Inside result:{}".format(a/b))
