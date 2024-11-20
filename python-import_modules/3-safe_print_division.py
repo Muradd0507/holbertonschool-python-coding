6e61cecf1b3b99f8a4c76fa6811abe97be4018bb
Muradd0507 <muradqulizade75@mail.com>
2024-11-20 11:19:06 -0500

    project6_4

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        if result is not None:
-            print("{} / {} = {}".format(a, b, result))
+    if result is not None:
+        print("{} / {} = {}".format(a, b, result))
     return result
 
