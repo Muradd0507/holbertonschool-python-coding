9d05ae6c994816da77a69857647ed90441cec524
Muradd0507 <muradqulizade75@gmail.com>
2024-11-21 17:02:53 +0400

    project8_3

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     m = 0
-    for i in a_dictionary:
-        if len(a_dictionary) > 0:
+    if len(a_dictionary) > 0:
+        for i in a_dictionary:
             if a_dictionary[i] > m:
                 m = a_dictionary[i]
-            for i, j in a_dictionary.items():
-                if j == m:
-                    return i
-        else:
-            return None
+        for i, j in a_dictionary.items():
+            if j == m:
+                return i
+    else:
+        return None
