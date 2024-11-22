235446d791eb7d2e1f16e47301cdffb9ca460fe2
Muradd0507 <muradqulizade75@gmail.com>
2024-11-22 21:01:07 +0400

    salam

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -6,14 +6,16 @@ if __name__ == "__main__":
     argv = sys.argv
     if len(argv) > 0:
         argv = argv[1:]
-    if len(argv) != 1 or len(argv) !=0:
+    if len(argv) != 1:
         print("{} arguments:".format(len(argv)))
+    if len(argv) == 0:
+        print("0 arguments.")
     else:
         print("1 argument:")
 
     for t, argument in enumerate(argv):
         if len(argv) == 0:
-            print("0 arguments.")
+            pass
         elif len(argv) == 1:
             print("{}: {}".format(t + 1, argument))
         else:
