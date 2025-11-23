751c1cffc783ba774bd8437eca8d8b035cc37656
Muradd0507 <muradqulizade75@gmail.com>
2025-11-23 19:10:04 +0000

    aaa

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -34,7 +34,7 @@ class Square:
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("#")
+                    print("#", end='')
                 print()
 
 
