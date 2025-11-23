6b75d943cb95a4ad0b18781b88cb1d22e74a16b1
Muradd0507 <muradqulizade75@gmail.com>
2025-11-23 18:54:15 +0000

    aaa

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,10 +15,10 @@ class Square:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
-
+    @property
     def size(self):
         return self.__size
-
+    @size.setter
     def size(self, value):
         self.__size = value
 
