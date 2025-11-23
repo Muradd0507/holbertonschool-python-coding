ca7946c4a43fa9b9e08a236317bb156af8bdc2e2
Muradd0507 <muradqulizade75@gmail.com>
2025-11-23 18:02:51 +0000

    aaa

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This code defines the size of Square
+"""
 class Square:
+    """
+    This is my first class
+    """
     def __init__(self, size):
         self.__size = size
