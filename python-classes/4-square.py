f652eabf3fe4a982b0c58fdb956c04c7d84501f8
Muradd0507 <muradqulizade75@gmail.com>
2025-11-23 19:09:27 +0000

    aaa

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -28,8 +28,15 @@ class Square:
         if (self.__size < 0):
             raise ValueError("size must be >= 0")
 
-    def area(self):
-        if (self.__size == 0):
+    def my_print(self):
+        if(self.__size == 0):
             print()
         else:
-            return self.__size * self.__size
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#")
+                print()
+
+
+    def area(self):
+        return self.__size * self.__size
