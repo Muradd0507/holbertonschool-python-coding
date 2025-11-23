f91bda26a5c6e9f818936b7ad86d3216d6b40709
Muradd0507 <muradqulizade75@gmail.com>
2025-11-23 19:05:43 +0000

    aaa

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This code defines the size of Square
+"""
+
+
+class Square:
+    """
+    This is my first class
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if (type(self.__size) is not int):
+            raise TypeError("size must be an integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        if (self.__size == 0):
+            print()
+        else:
+            return self.__size * self.__size
